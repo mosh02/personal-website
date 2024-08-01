f0aec001564645834c1756ad7859d748402ad163
mosh <158522662+mosh02@users.noreply.github.com>
2024-07-31 21:30:13 -0400

    added js file

---
+++ js/javascript.js
@@ -0,0 +1,17 @@
+console.log("hello, world!")
+
+console.log((4+6+9)/77)
+
+
+let a = 10;
+console.log(a)
+let b = 7 * a;
+console.log(b)
+
+let MAX = 57;
+let actual = MAX - 13;
+let percentage = actual / MAX;
+
+console.log(percentage)
+
+alert(10)
